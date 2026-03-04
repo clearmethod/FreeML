b250e9c7483e5c98d7db8559d50c7ba33acde8f6
ClearMethod <no@email.com>
2026-03-04 18:50:06 +0000

    Relu matrix handler for GPU

--- ActivationLibrary/Relu.h
+++ ActivationLibrary/Relu.h
@@ -3,12 +3,42 @@
 #include <algorithm>
 
 #include "ActivationFunction.h"
+#ifdef _WIN32
+#include <MatrixLibrary/GPU/DirectX11/MatrixDX11_Functions.h>
+#endif
 
 template<class T>
 class Relu : public ActivationFunction<T>
 {
 public:
+    using ActivationFunction<T>::activateMat;
+    using ActivationFunction<T>::derivativeMat;
+
     const char* Name()  override { return "relu"; }
     T activate(T x)  override { return std::max(T(0), x); }
     T derivative(T x)  override { return x > T(0) ? T(1) : T(0); }
+
+    template<class Mat>
+    void activateMat(Mat* _matOut, Mat* _matIn)
+    {
+        ReluMat(_matOut, _matIn);
+    }
+
+    template<class Mat>
+    void activateMat(Mat* _matOut,
+                     Mat* _matIn,
+                     ThreadPool* _pool)
+    {
+        (void)_pool;
+        ReluMat(_matOut, _matIn);
+    }
+
+    template<class Mat>
+    void derivativeMat(Mat* _matOut,
+                       Mat* _matIn,
+                       Mat* _notused = nullptr)
+    {
+        (void)_notused;
+        ReluDerivativeMat(_matOut, _matIn);
+    }
 };
